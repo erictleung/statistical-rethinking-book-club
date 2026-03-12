26f9d7248a616f8a25f6679455a688260e8bd40f
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-03-12 17:07:07 -0400

    Update R Chapter 13 file

--- R/r_chapter13.R
+++ R/r_chapter13.R
@@ -38,3 +38,33 @@ m13.2 <- ulam(
 
 # R code 13.4
 compare(m13.1, m13.2)
+
+# WAIC
+# SE
+# dWAIC
+
+
+# R code 13.5
+
+# Extract Stan samples
+post <- extract.samples(m13.2)
+
+# Compute mean intercept for each tank
+# Also transform to probability with logistic
+d$propsurv.est <- logistic(apply(post$a, 2, mean))
+
+# Display raw proportions surviving in each tank
+plot(
+  d$propsurv,
+  ylim = c(0, 1),
+  pch = 16,
+  xaxt = "n",
+  xlab = "Tank",
+  ylab = "Proportion survival",
+  col = rangi2
+)
+axis(
+  1,
+  at = c(1, 16, 32, 48),
+  labels = c(1, 16, 32, 48)
+)
